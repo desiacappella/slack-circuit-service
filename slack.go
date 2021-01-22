40f3bd16f4b2eb15912b7f75c99091fd6962df6d
Sarang Joshi <sarangj@msn.com>
2021-01-21 17:34:04 -0800

    Adding team officers and creating team channels

--- slack.go
+++ slack.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/slack-go/slack"
 )
@@ -76,4 +77,112 @@ outer:
 
 func slackInviteOfficers(emails []string) {
 	// Not possible without Enterprise Grid. lovely
+}
+
+func slackCollectChannels() (allChannels []slack.Channel) {
+	var cursor string
+	for {
+		var channels []slack.Channel
+		var err error
+		channels, cursor, err = api.GetConversations(&slack.GetConversationsParameters{Types: []string{typePrivateChannel}, Cursor: cursor})
+		if err != nil {
+			epanic(err, "Can't get channels for user")
+		}
+		allChannels = append(allChannels, channels...)
+
+		if len(cursor) == 0 {
+			break
+		}
+	}
+	return allChannels
+}
+
+// Returns true if newly added, false if already existing
+func addEmailToChannel(channel slack.Channel, email string, emailToIds map[string]string) string {
+	liaisonID, ok := emailToIds[email]
+	if !ok {
+		user, err := api.GetUserByEmail(email)
+		if err != nil {
+			if err.Error() == "users_not_found" {
+				return "NEED TO ADD " + email
+			}
+			epanic(err, "Can't get user by email "+email)
+		}
+
+		emailToIds[email] = user.ID
+		liaisonID = user.ID
+	}
+
+	// Is the liaison already in?
+	members, _, err := api.GetUsersInConversation(&slack.GetUsersInConversationParameters{ChannelID: channel.ID})
+	if err != nil {
+		epanic(err, "can't list members")
+	}
+	found := false
+	for _, mem := range members {
+		if mem == liaisonID {
+			found = true
+			break
+		}
+	}
+	if !found {
+		_, err := api.InviteUsersToConversation(channel.ID, liaisonID)
+		if err != nil {
+			epanic(err, "Can't invite liaison")
+		}
+	}
+
+	if found {
+		return "already there"
+	}
+	return "newly added"
+}
+
+func slackTeamChannels(teams []Team) {
+	// First check in unusual
+	// Random heuristics:
+	// - stop once you see a column
+	// - remove A Cappella
+
+	allChannels := slackCollectChannels()
+	emailToIds := make(map[string]string)
+
+	fmt.Printf("%-25v | %-25v | %-25v | %-25v | %-25v\n", "Team Name", "Channel Name", "Status", "Liaison", "Captain")
+	for _, team := range teams {
+		channelName, ok := unusualMapping[team.Name]
+		if !ok {
+			channelName = strings.ReplaceAll(strings.TrimSpace(strings.Split(strings.ReplaceAll(strings.ToLower(team.Name), "a cappella", ""), ":")[0]), " ", "-")
+		}
+		channelName = "team-" + channelName
+
+		var channel slack.Channel
+		for _, channel = range allChannels {
+			if channel.Name == channelName {
+				break
+			}
+		}
+
+		var channelStatus string
+		if len(channel.ID) == 0 {
+			// Create channels
+			channel, err := api.CreateConversation(channelName, true)
+			time.Sleep(time.Second)
+			if err != nil {
+				channelStatus = err.Error()
+			} else {
+				channelStatus = "created successfully " + channel.ID
+			}
+		} else {
+			channelStatus = "already created"
+		}
+
+		// Add liaison
+		addedLiai := false // addEmailToChannel(channel, team.Liaison.Email, emailToIds)
+
+		// Add captain
+		addCapResult := addEmailToChannel(channel, team.Captain.Email, emailToIds)
+
+		fmt.Printf("%-25v | %-25v | %-25v | %-25v | %-25v\n", team.Name, channelName, channelStatus,
+			fmt.Sprintf("added %s (new: %v)", team.Liaison.Name, addedLiai), fmt.Sprintf("added %s (res: %s)", team.Captain.Name, addCapResult))
+	}
 }
--- util.go
+++ util.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -17,4 +20,70 @@ func getToken(filename string) string {
 		panic("Unable to find user token")
 	}
 	return strings.TrimSpace(string(b))
+}
+
+// Person is
+type Person struct {
+	Name  string
+	Email string
+}
+
+// Team is
+type Team struct {
+	Name       string
+	University string
+	Liaison    Person
+	Captain    Person
+	Officers   []Person
+}
+
+var unusualMapping = map[string]string{
+	"New York Masti": "masti",
+}
+
+func parseTeams() []Team {
+	teamsFile, err := os.OpenFile("captains.csv", os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	defer teamsFile.Close()
+
+	r := csv.NewReader(teamsFile)
+
+	teams := []Team{}
+
+	// Ignore headers
+	r.Read()
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+
+		team := Team{
+			Name:       record[0],
+			University: record[1],
+			Liaison: Person{
+				record[2],
+				record[3],
+			},
+			Captain: Person{
+				Name:  record[4],
+				Email: record[5],
+			},
+		}
+
+		// Parse out officers
+		for i := 6; i < len(record); i += 2 {
+			if len(strings.TrimSpace(record[i])) == 0 {
+				break
+			}
+
+			team.Officers = append(team.Officers, Person{Name: record[i], Email: record[i+1]})
+		}
+
+		teams = append(teams, team)
+	}
+
+	return teams
 }
